3675ff82668bfcd15ff36e3492cb7f00881033f8
evan <evan.kuo@deltaww.com>
2023-12-18 11:47:44 +0800

    init project

---
+++ android/app/src/main/java/com/ygk/q8demo/MainActivity.java
@@ -0,0 +1,36 @@
+package com.ygk.q8demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.ygk.q8demo.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'q8demo' library on application startup.
+    static {
+        System.loadLibrary("q8demo");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'q8demo' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
